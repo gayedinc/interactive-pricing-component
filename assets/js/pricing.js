160ff162331c772efa21558aa84a4c7785662d33
[Gaye Dinç] <gayedinc190@gmail.com>
2024-10-22 12:28:55 +0300

    interactive-pricing-component yüklendi

---
+++ assets/js/pricing.js
@@ -0,0 +1,16 @@
+const fiyat = 0.00016;
+
+function hesapla() {
+  const gosterim = Number(rangeInput.value) * 1000;
+  let toplamFiyat = Math.round(gosterim * fiyat);
+  if (yillikOdeme.checked) {
+    toplamFiyat = (toplamFiyat * 12) * 0.75;
+  }
+  priceAmountTxt.innerText = toplamFiyat + ' USD';
+  pageviewScore.innerText = rangeInput.value * 2 + 'K PAGEVIEWS';
+}
+
+rangeInput.addEventListener('input', hesapla);
+yillikOdeme.addEventListener('click', hesapla);
+
+hesapla();
